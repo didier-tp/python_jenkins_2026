c9e4fadcb1703bc1db5dfcae451796c38a040978
didier defrance <didier@d-defrance.fr>
2026-07-27 16:28:04 +0200

    sans_erreur

--- mensualite.py
+++ mensualite.py
@@ -1,3 +1,3 @@
 def mensualite(montant,nb_mois,taux_interet_pct):
     taux_mensuel = (taux_interet_pct / 100) / 12
-    return ( montant * taux_mensuel)  / (1 - (1+taux_mensuel)** (- nb_mois)) + 2
+    return ( montant * taux_mensuel)  / (1 - (1+taux_mensuel)** (- nb_mois))
